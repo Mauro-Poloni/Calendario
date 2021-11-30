5867dba90990fc4e4f7f6f90255ef9b16bdcd292
Mauro Poloni <89918269+Mauro-Poloni@users.noreply.github.com>
2021-11-30 13:01:57 -0300

    Proyecto: Calendario moderno con Reloj
    Visita el proyecto en: 
    https://portafolio-mauro-poloni-calendario.netlify.app/

---
+++ script.js
@@ -0,0 +1,105 @@
+//------------Calendario----------------------
+
+const date = new Date();
+
+const renderCalendar = () => {
+    date.setDate(1);
+
+    const monthDays = document.querySelector(".days");
+
+    const lastDay = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+    ).getDate();
+    
+    const prevLastDay = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        0
+    ).getDate();
+
+    const firstDayIndex = date.getDay();
+
+    const lastDayIndex = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+    ).getDay();
+
+    const nextDays = 7 - lastDayIndex - 1;
+
+    const months = [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre"
+    ];
+
+    document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+    document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+    let days = "";
+
+ for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+    document.querySelector(".prev").addEventListener("click", () => {
+        date.setMonth(date.getMonth() -1);
+        renderCalendar();
+    });
+
+    document.querySelector(".next").addEventListener("click", () => {
+        date.setMonth(date.getMonth() + 1);
+        renderCalendar();
+    });
+
+    renderCalendar();
+  
+
+//------------------Reloj-----------------------------------
+
+"use strict";
+const addZeros = n => {
+	if (n.toString().length < 2) return "0".concat(n);
+	return n;
+}
+const actualizarHora = ()=>{
+	const time = new Date();
+	let hora = addZeros(time.getHours());
+	let min = addZeros(time.getMinutes());
+	let seg = addZeros(time.getSeconds());
+	document.querySelector(".hora").textContent = hora + ":";
+	document.querySelector(".min").textContent = min + ":";
+	document.querySelector(".seg").textContent = seg;
+}
+actualizarHora()
+setInterval(actualizarHora,1000)
+
